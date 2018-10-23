26b544939384ad72ab084ff69f595989834e1fc3
elspetha5 <elspetha@gmail.com>
2018-10-23 12:09:53 -0700

    code update

--- dsp-splash-page/src/components/NavBar/NavBar.js
+++ dsp-splash-page/src/components/NavBar/NavBar.js
@@ -1,32 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./navbar.css";
 
-const NavBar = props => {
-    return (
-        <div id="navbar">
-            <Link to="/" className="link" id="banner">
-                <div id="logobox">
-                    <div className="logo" id="logo">dsp...</div>
-                    <div className="logo" id="under">under</div>
-                    <div className="logo" id="construction">contruction</div>
+class NavBar extends Component {
+    state = {
+    };
+
+    render() {
+        return (
+            <div id="navbar">
+                <Link to="/" className="link" id="banner">
+                    <div id="logobox">
+                        <div className="logo" id="logo">dsp...</div>
+                        <div className="logo" id="under">under</div>
+                        <div className="logo" id="construction">contruction</div>
+                    </div>
+                </Link>
+
+                <div id="navLine"></div>
+
+                <div id="questions">
+                    <Link to="/what" className="link question">
+                        What Are We Building?
+                    </Link>
+                    <Link to="/who" className="link question">
+                        Who Is It For?
+                    </Link>
                 </div>
-            </Link>
 
-            <div id="navLine"></div>
 
-            <div id="questions">
-                <Link to="/what" className="link question">
-                    What Are We Building?
-                </Link>
-                <Link to="/who" className="link question">
-                    Who Is It For?
-                </Link>
             </div>
-
-
-        </div>
-    )
+        )
+    };
 };
 
 export default NavBar;
