ae88176f557e4ee4dee894de94355a4f827da2c5
elspetha5 <elspetha@gmail.com>
2018-11-07 15:37:15 -0700

    navbar responsiveness started

--- dsp-splash-page/src/components/NavBar/NavBar.js
+++ dsp-splash-page/src/components/NavBar/NavBar.js
@@ -3,10 +3,8 @@ import { Link } from "react-router-dom";
 import "./navbar.css";
 
 import contents from "../../content.json"
-import logo from "./dsp-logo.png";
-import logo2 from "./dsp-logo-blue-circle-white.png";
-import logo3 from "./dsp-logo-white-circle-teal.png";
-import logo4 from "./dsp-logo-blue-circle-white-no-elipsis.png";
+import logo from "./dsp-logo-blue-circle-white-no-elipsis.png";
+import stamp from "./blackStamp.png";
 
 class NavBar extends Component {
     state = {
@@ -20,27 +18,36 @@ class NavBar extends Component {
     render() {
         return (
             <div id="navbar">
-                {/* <Link to="/WHAT" className="link" id="banner">
-                    <div id="logobox">
-                        <img src={logo} alt="logo" id="logo"/>
-                        <div id="construction">under construction</div>
-                    </div>
-                </Link>
-
-                <div id="navLine"></div> */}
 
                 <div id="questions">
 
-                    <Link to="/WHAT" className="link" id="leftLogo">
-                        <img src={logo4} alt="logo" id="logo" />
-                    </Link>
+                    <nav className="navbar navbar-expand-lg navbar-light ">
 
-                    <div id="questionBox">
-                        {this.state.contents.map(content => (
-                            <Link to={"/" + content.question} className="link question">{content.question}</Link>
-                        ))}
-                    </div>
+                        <Link to="/WHAT" className="link">
+                            <img src={logo} alt="logo" id="logo" />
+                        </Link>
+
+                        <button id="hamburger" className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                            <div className="navbar-nav">
+                                {this.state.contents.map(content => (
+                                    <Link to={"/" + content.question} className="nav-item link question">{content.question}</Link>
+                                ))}
+                            </div>
+                        </div>
+
+                        <img id="constStamp" src={stamp} alt="underConst" />
+
+                    </nav>
+
+
+
+
                 </div>
+
 
 
             </div>
--- dsp-splash-page/src/pages/What/What.js
+++ dsp-splash-page/src/pages/What/What.js
@@ -3,7 +3,6 @@ import "./what.css";
 
 /* import QPage from "../../components/QPage"; */
 import LeftIdea from "../../components/LeftIdea";
-import RightIdea from "../../components/RightIdea";
 
 import contents from "../../content.json";
 import ideas from "../../what.json"
@@ -27,52 +26,14 @@ class What extends Component {
                     <div className="thesis">Design Service Professionals, PLLC is an all-virtual design collaborative specializing in design services for the AEC Industry. Leveraging a diverse reservoir of design professionals, our staff augmentation program, coupled with an eclectic mix of industry experts, allows us to assemble the right team at the right time for our clients & AEC partners. By tapping into the dsp ecosystem, our clients, design-build partners, large project teams, and boutique firms alike will benefit from our unparalleled delivery of high quality design.</div>
                 </div>
 
-                {this.state.ideas.map(idea1 => (
-                    <LeftIdea
-                        idea={idea1.idea}
-                        verbiage={idea1.verbiage}
-                    />
-                ))}
-
-                {/* <LeftIdea
-                    idea={this.state.ideas[0].idea}
-                    verbiage={this.state.ideas[0].verbiage}
-                />
-
-                <RightIdea 
-                    idea={this.state.ideas[1].idea}
-                    verbiage={this.state.ideas[1].verbiage}
-                />
-
-                <LeftIdea
-                    idea={this.state.ideas[2].idea}
-                    verbiage={this.state.ideas[2].verbiage}
-                />
-
-                <RightIdea 
-                    idea={this.state.ideas[3].idea}
-                    verbiage={this.state.ideas[3].verbiage}
-                />
-
-                <LeftIdea
-                    idea={this.state.ideas[4].idea}
-                    verbiage={this.state.ideas[4].verbiage}
-                />
-
-                <RightIdea 
-                    idea={this.state.ideas[5].idea}
-                    verbiage={this.state.ideas[5].verbiage}
-                />
-
-                <LeftIdea
-                    idea={this.state.ideas[6].idea}
-                    verbiage={this.state.ideas[6].verbiage}
-                />
-
-                <RightIdea 
-                    idea={this.state.ideas[7].idea}
-                    verbiage={this.state.ideas[7].verbiage}
-                /> */}
+                <div className="wrapper">
+                    {this.state.ideas.map(idea1 => (
+                        <LeftIdea
+                            idea={idea1.idea}
+                            verbiage={idea1.verbiage}
+                        />
+                    ))}
+                </div>
 
             </div>
         )
