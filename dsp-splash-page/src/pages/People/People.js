b8374b8e50ef55d3cc2fda619b2a7b01e16b5440
elspetha5 <elspetha@gmail.com>
2019-02-20 15:00:50 -0700

    people page updates

--- dsp-splash-page/src/pages/People/People.js
+++ dsp-splash-page/src/pages/People/People.js
@@ -15,17 +15,58 @@ class People extends Component {
 
     render() {
         return (
-            <div>
+            <div id="peopleBackground">
                 <div id="peopleTop">
                     <div id="aboutUs">About Us</div>
                 </div>
 
                 <div id="peopleWrapper">
                     {this.state.people.map(person => (
-                        <img src={person.picture} alt="person" className="peoplePic"/>
+                        <Popup
+                            trigger={<img src={person.picture} alt="person" className="peoplePic" />}
+                            position="right center"
+                            closeOnDocumentClick
+                            contentStyle={{
+                                "border": "none",
+                                "border-radius": "2%"
+                            }}
+                            
+                        >
+                            <div className="peopleTooltip">
+                                <div className="peopleTitle">{person.title} </div>
+                                <div className="peopleCred">{person.credentials}</div>
+                                <div className="peopleAbout">
+                                    {person.about}
+                            </div>
+                            </div>
+                        </Popup>
+
                     ))}
 
-                    
+                </div>
+
+                <div id="peopleWrapper">
+                {this.state.people.map(person => (
+                        <Popup
+                            trigger={<img src={person.picture} alt="person" className="peoplePicRound" />}
+                            position="right center"
+                            closeOnDocumentClick
+                            contentStyle={{
+                                "border": "none",
+                                "border-radius": "2%"
+                            }}
+                            
+                        >
+                            <div className="peopleTooltip">
+                                <div className="peopleTitle">{person.title} </div>
+                                <div className="peopleCred">{person.credentials}</div>
+                                <div className="peopleAbout">
+                                    {person.about}
+                            </div>
+                            </div>
+                        </Popup>
+
+                    ))}
                 </div>
 
             </div>
