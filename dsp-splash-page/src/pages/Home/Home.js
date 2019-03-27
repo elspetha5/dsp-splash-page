74a3a96a7184f3aaabb3a4a127e6496e7ff80a39
elspetha5 <elspetha@gmail.com>
2019-03-27 16:23:49 -0700

    home responsiveness

--- dsp-splash-page/src/pages/Home/Home.js
+++ dsp-splash-page/src/pages/Home/Home.js
@@ -18,25 +18,17 @@ class Home extends Component {
     render() {
         return (
             <div id="homepage">
+                <div id="homeSlant"></div>
+
                 <div id="homeLogo" className="homeHalf">
                     <img src={logo} alt="logo" id="homeLogoIcon" />
                 </div>
-
-                <div id="homeSlant"></div>
 
                 <div id="homeWords" className="homeHalf">
                     <div className="homeTagline">Design Service Professionals . . .</div>
                     <div className="homeTagline">emerging at the intersection</div>
                     <div className="homeTagline">of design + delivery</div>
                 </div>
-
-                {/* <div className="taglineBackground">
-                    <div className="taglineBox">
-                        <div className="taglineThesis">Design Service Professionals . . .</div>
-                        <div className="taglineThesis">emerging at the intersection</div>
-                        <div className="taglineThesis">of design + delivery</div>
-                    </div>
-                </div> */}
 
             </div>
         )
