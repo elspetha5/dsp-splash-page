9f57037dc3306d0f810e967a2cbe702ed0c67f91
elspetha5 <elspetha@gmail.com>
2019-03-28 15:53:07 -0700

    started building static people page

--- dsp-splash-page/src/pages/People/People.js
+++ dsp-splash-page/src/pages/People/People.js
@@ -7,41 +7,51 @@ import Cluster from "./Cluster";
 import "./people.css";
 
 import people from "../../JSON/People/people.json";
+import sixPeople from "../../JSON/People/sixPeople.json";
 
 class People extends Component {
     constructor(props) {
         super(props);
         this.state = {
             people,
+            sixPeople,
 
-            backButton: "backNoDisplay",
+            // backButton: "backNoDisplay",
 
-            peopleThree1: "peopleAsThree",
-            peopleThree2: "peopleAsThree",
-            peopleThree3: "peopleAsThree",
+            // peopleThree1: "peopleAsThree",
+            // peopleThree2: "peopleAsThree",
+            // peopleThree3: "peopleAsThree",
 
-            department1: "cluster",
-            department2: "cluster",
-            department3: "cluster",
+            // department1: "cluster",
+            // department2: "cluster",
+            // department3: "cluster",
 
-            visibility1: "visible",
-            visibility2: "visible",
-            visibility3: "visible",
+            // visibility1: "visible",
+            // visibility2: "visible",
+            // visibility3: "visible",
 
-            depVisibility: "visible",
-            hiddenPic: "hiddenPic"
+            // depVisibility: "visible",
+            // hiddenPic: "hiddenPic"
         };
-        this.straightenCluster1 = this.straightenCluster1.bind(this);
-        this.straightenCluster2 = this.straightenCluster2.bind(this);
-        this.straightenCluster3 = this.straightenCluster3.bind(this);
-        this.back = this.back.bind(this);
+        // this.straightenCluster1 = this.straightenCluster1.bind(this);
+        // this.straightenCluster2 = this.straightenCluster2.bind(this);
+        // this.straightenCluster3 = this.straightenCluster3.bind(this);
+        // this.back = this.back.bind(this);
     }
 
     componentDidMount() {
-        window.scrollTo(0, 0)
+        window.scrollTo(0, 0);
+        this.shuffleArray(sixPeople);
     };
 
-    back = () => {
+    shuffleArray = array => {
+        for (let i = array.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        };
+    };
+
+    /* back = () => {
         this.setState({
             backButton: "backNoDisplay",
 
@@ -135,17 +145,25 @@ class People extends Component {
         };
 
         return;
-    };
+    }; */
 
     render() {
         return (
             <div id="peopleBackground">
                 <div id="peopleTop">
                     <div id="aboutUs">we are . . . <span id="aboutUsBreak">design service professionals</span></div>
-                    <Link to="#" id={this.state.backButton} className="backToPeople" onClick={this.back}><i class="fas fa-chevron-left"></i> back</Link>
+                    {/* <Link to="#" id={this.state.backButton} className="backToPeople" onClick={this.back}><i class="fas fa-chevron-left"></i> back</Link> */}
                 </div>
 
-                <Cluster
+                <div id="peoplePicBox">
+                    {this.state.sixPeople.map(person => (
+                        <div>
+                            <img src={person.picture} alt="person" className="peoplePic" />
+                        </div>
+                    ))}
+                </div>
+                
+                {/* <Cluster
                     num={1}
                     dep="admin"
                     background={this.state.peopleThree1}
@@ -182,208 +200,7 @@ class People extends Component {
                     array={this.state.people}
                     clusterPic={this.state.department3}
                     hiddenPic={this.state.hiddenPic}
-                />
-
-                {/* <div className={`peopleThree ${this.state.peopleThree1}`} id="peopleThree1">
-                    <div id={this.state.department1} className={`peopleFadeIn ${this.state.visibility1}`} onClick={this.straightenCluster1}>
-                        <div className={`depTitle ${this.state.depVisibility}`}>Department 1</div>
-                        {this.state.people.map((person, i) => (
-                            (i < 5)
-                                ?
-                                <img src={person.picture} alt="person" className={`clusterPic ${this.state.department1 + i}`} />
-                                :
-                                <img src={person.picture} alt="person" className={`clusterPic ${this.state.hiddenPic}`} />
-                        ))}
-
-                    </div>
-                </div>
-
-                <div className={`peopleThree ${this.state.peopleThree2}`} id="peopleThree2">
-                    <div id={this.state.department2} className={`peopleFadeIn ${this.state.visibility2}`} onClick={this.straightenCluster2}>
-                        <div className={`depTitle ${this.state.depVisibility}`}>Department 2</div>
-                        {this.state.people.map((person, i) => (
-                            (i < 5)
-                                ?
-                                <img src={person.picture} alt="person" className={`clusterPic ${this.state.department2 + i}`} />
-                                :
-                                <img src={person.picture} alt="person" className={`clusterPic ${this.state.hiddenPic}`} />
-                        ))}
-                    </div>
-                </div>
-
-                <div className={`peopleThree ${this.state.peopleThree3}`} id="peopleThree3">
-                    <div id={this.state.department3} className={`peopleFadeIn ${this.state.visibility3}`} onClick={this.straightenCluster3}>
-                        <div className={`depTitle ${this.state.depVisibility}`}>Department 3</div>
-                        {this.state.people.map((person, i) => (
-                            (i < 5)
-                                ?
-                                <img src={person.picture} alt="person" className={`clusterPic ${this.state.department3 + i}`} />
-                                :
-                                <img src={person.picture} alt="person" className={`clusterPic ${this.state.hiddenPic}`} />
-                        ))}
-                    </div>
-                </div> */}
-
-                {/* <div id={this.state.department1} className={`clusterOne ${this.state.visibility1}`} onClick={this.straightenClusterOne}>
-                    <div className={`depTitle ${this.state.depVisibility}`}>Department 1</div>
-                    {this.state.people.map((person, i) => (
-                        (i < 5)
-                            ?
-                                <img src={person.picture} alt="person" className={`clusterPic ${this.state.department1 + i}`} />
-                            :
-                                <img src={person.picture} alt="person" className={`clusterPic hiddenPic`} />
-                    ))}
-
-                </div>
-
-
-                <div id={this.state.department2} className={`clusterTwo ${this.state.visibility2}`} onClick={this.straightenClusterTwo}>
-                    <div className={`depTitle ${this.state.depVisibility}`}>Department 2</div>
-                    {this.state.people.map((person, i) => (
-                        (i < 5)
-                            ?
-                                <img src={person.picture} alt="person" className={`clusterPic ${this.state.department2 + i}`} />
-                            :
-                                <img src={person.picture} alt="person" className={`clusterPic hiddenPic`} />
-                    ))}
-                </div>
-
-
-                <div id={this.state.department3} className={`clusterThree ${this.state.visibility3}`} onClick={this.straightenClusterThree}>
-                    <div className={`depTitle ${this.state.depVisibility}`}>Department 3</div>
-                    {this.state.people.map((person, i) => (
-                        (i < 5)
-                            ?
-                                <img src={person.picture} alt="person" className={`clusterPic ${this.state.department3 + i}`} />
-                            :
-                                <img src={person.picture} alt="person" className={`clusterPic hiddenPic`} />
-                    ))}
-                </div> */}
-
-                {/* <div id="peopleWrapper">
-                    {this.state.people.map(person => (
-                        <Popup
-                            trigger={<img src={person.picture} alt="person" className="peoplePic" />}
-                            position="right center"
-                            closeOnDocumentClick
-                            contentStyle={{
-                                "border": "none",
-                                "border-radius": "2%"
-                            }}
-
-                        >
-                            <div className="peopleTooltip">
-                                <div className="peopleTitle">{person.title} </div>
-                                <div className="peopleCred">{person.credentials}</div>
-                                <div className="peopleAbout">
-                                    {person.about}
-                                </div>
-                            </div>
-                        </Popup>
-
-                    ))}
-
-                </div>
-
-                <div id="peopleWrapper">
-                    {this.state.people.map((person, i) => (
-                        <Panel eventKey={i} className="inline">
-                            <Panel.Title toggle>
-                                <img src={person.picture} alt="person" className="peoplePicRound" />
-                            </Panel.Title>
-
-                            <Panel.Body collapsible className="peopleExpand">
-                                <div className="peopleTooltip">
-                                    <div className="peopleTitle">{person.title} </div>
-                                    <div className="peopleCred">{person.credentials}</div>
-                                    <div className="peopleAbout">
-                                        {person.about}
-                                    </div>
-                                </div>
-                            </Panel.Body>
-                        </Panel>
-
-                    ))}
-                </div>
-
-                <div id="peopleWrapper">
-                    <div className="nucleusTitle">Founders</div>
-                    <div className="peopleLine"></div>
-                    {this.state.twoPeople.map(person => (
-                        <Popup
-                            trigger={<img src={person.picture} alt="person" className="peoplePic" />}
-                            position="right center"
-                            closeOnDocumentClick
-                            contentStyle={{
-                                "border": "none",
-                                "border-radius": "2%"
-                            }}
-
-                        >
-                            <div className="peopleTooltip">
-                                <div className="peopleTitle">{person.title} </div>
-                                <div className="peopleCred">{person.credentials}</div>
-                                <div className="peopleAbout">
-                                    {person.about}
-                                </div>
-                            </div>
-                        </Popup>
-
-                    ))}
-
-                    <div className="nucleusTitle">Management</div>
-                    <div className="peopleLine"></div>
-                    {this.state.threePeople.map((person, i) => (
-                        <Panel eventKey={i} className="inline">
-                            <Panel.Title toggle>
-                                <img src={person.picture} alt="person" className="peoplePicRound" />
-                            </Panel.Title>
-
-                            <Panel.Body collapsible className="peopleExpand">
-                                <div className="peopleTooltip">
-                                    <div className="peopleTitle">{person.title} </div>
-                                    <div className="peopleCred">{person.credentials}</div>
-                                    <div className="peopleAbout">
-                                        {person.about}
-                                    </div>
-                                </div>
-                            </Panel.Body>
-                        </Panel>
-
-                    ))}
-
-                    <div className="nucleusTitle">Department</div>
-                    <div className="peopleLine"></div>
-                    {this.state.fivePeople.map(person => (
-                        <Popup
-                            trigger={<img src={person.picture} alt="person" className="peoplePic" />}
-                            position="right center"
-                            closeOnDocumentClick
-                            contentStyle={{
-                                "border": "none",
-                                "border-radius": "2%"
-                            }}
-
-                        >
-                            <div className="peopleTooltip">
-                                <div className="peopleTitle">{person.title} </div>
-                                <div className="peopleCred">{person.credentials}</div>
-                                <div className="peopleAbout">
-                                    {person.about}
-                                </div>
-                            </div>
-                        </Popup>
-
-                    ))}
-                </div>
-
-                {this.state.fivePeople.map(person => (
-                    <LeftIdea
-                        idea={<img src={person.picture} alt="person" className="peopleLeft" />}
-                        verbiage={person.about}
-                    />
-                ))} */}
-
+                /> */}
 
             </div>
         )
