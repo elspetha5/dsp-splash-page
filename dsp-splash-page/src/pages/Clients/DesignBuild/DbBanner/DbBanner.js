5aa57a98a4991b51969286d44703449e18426a08
elspetha5 <elspetha@gmail.com>
2019-05-01 16:11:44 -0700

    building db banner

---
+++ dsp-splash-page/src/pages/Clients/DesignBuild/DbBanner/DbBanner.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./dbBanner.css";
+
+import whiteLogo from "./dsplogo-white.png";
+
+const DbBanner = () => {
+    return (
+        <div id="dbBannerWrapper" className="charcoalish-background slateWhite-text">
+            <div id="dbBannerTitle"><span id="dbBannerDB" className="charcoalish-background">DESIGN BUILD</span></div>
+            <div id="dashedBorder">
+
+                <div id="ccLogo">
+                    <div id="dbLogoWrapper">
+                        <img src={whiteLogo} alt="white logo" id="dbBannerLogo" />
+                    </div>
+                    <div className="ccText yellow-text">CONSTRUCTABILITY</div>
+                    <div className="ccText yellow-text">CONCEPT</div>
+                </div>
+
+                <div id="dbPlus" className="yellow-text"><i class="fas fa-plus"></i></div>
+
+                <div id="yourBuild">
+                    <div id="your">YOUR</div>
+                    <div id="build">BUILD</div>
+                </div>
+
+                <div id="equals" className="yellow-text"><i class="fas fa-equals"></i></div>
+
+                <div id="ipd">
+                    <div id="IPD" className="salmon-text">IPD</div>
+                    <div id="integ" className="yellow-text">INTEGRATED</div>
+                    <div id="projDel" className="yellow-text">PROJECT DELIVERY</div>
+                </div>
+
+            </div>
+        </div>
+    )
+};
+
+export default DbBanner;
--- dsp-splash-page/src/pages/LogIn/Login.js
+++ dsp-splash-page/src/pages/LogIn/Login.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import "./login.css";
 
+import DbBanner from "../Clients/DesignBuild/DbBanner";
+
 class Login extends Component {
     state = {
 
@@ -13,81 +15,10 @@ class Login extends Component {
     render() {
         return (
             <div>
-                
+                <DbBanner />
             </div>
         )
     };
 };
 
 export default Login;
-
-// import React, { Component } from "react";
-// import { Link } from "react-router-dom";
-// import { Navbar, Nav, NavItem, MenuItem, NavDropdown } from "react-bootstrap";
-// import { LinkContainer } from "react-router-bootstrap";
-// import "./navbar.css";
-
-// import logo from "./dspLogo.png";
-// import stamp from "./under-construction-circle.png";
-
-// class NavBar extends Component {
-//     state = {
-//         buttons: [
-//             { to: "/approach", text: "approach" },
-//             { to: "/about/people", text: "about" },
-//             { to: "/clients", text: "clients" },
-//             { to: "/designservices", text: "services" },
-//             { to: "/joinus", text: "ecosystem" }
-//         ],
-//         footer: ["hello@dsp.design", "Join us", "Design Service Professionals, PLLC 2019 All rights reserved"]
-//     };
-
-
-
-//     render() {
-//         return (
-//             <div>
-//                 {/* <Navbar collapseOnSelect> */}
-//                     <div id="flexNav" className="lightGrey-background">
-
-//                         <div id="flexImage">
-//                             <Link to="/"><img src={logo} alt="logo" id="flexLogo" /></Link>
-//                         </div>
-
-//                         <div id="flexFooter">
-//                             <div id="flexFooterWords">
-//                                 <a className="link" id="emailAddress" href="mailto:hello@dsp.design"><i class="far fa-envelope"></i> hello@dsp.design </a>
-//                                 <Link classname="link" id="footerJoin" to="/joinus"><i class="far fa-handshake"></i> Join us</Link>
-//                                 <div id="flexPLLC">Design Service Professionals, PLLC<span id="copyRights"> <i class="far fa-copyright"></i> 2019 <span id="rights">All rights reserved</span></span></div>
-//                             </div>
-//                         </div>
-
-//                         <div id="flexWords">
-//                             {/* <Navbar.Collapse> */}
-//                                 {/* <Nav id="flexQs"> */}
-//                                     {this.state.buttons.map((button, i) => (
-//                                         <Link to={button.to}>{button.text}</Link>
-//                                         // <LinkContainer to={button.to}>
-//                                             // <NavItem eventKey={`${i + 1}`} >{button.text}</NavItem>
-//                                         // </LinkContainer>
-//                                     ))}
-//                                 {/* </Nav> */}
-//                             {/* </Navbar.Collapse> */}
-//                         </div>
-
-//                         {/* <Navbar.Header id="flexHeader">
-//                             <Navbar.Toggle id="flexHamburger" />
-//                         </Navbar.Header> */}
-
-//                     </div>
-//                 {/* </Navbar> */}
-
-
-//                 <img id="constStamp" src={stamp} alt="underConst" />
-
-//             </div>
-//         )
-//     };
-// };
-
-// export default NavBar;
