a8075c0bc9f8f59ad9e016af14fa1d54bf4729cf
elspetha5 <elspetha@gmail.com>
2019-05-30 11:43:34 -0700

    margin on slantTop

--- dsp-splash-page/src/pages/JoinUs/JobList/JobList.js
+++ dsp-splash-page/src/pages/JoinUs/JobList/JobList.js
@@ -26,86 +26,78 @@ class JobList extends Component {
                     title="dsp.careers"
                 />
 
-                {this.state.jobs.map((job, i) => (
-                    (i < 2)
-                        ? <div className="jobWrapper">
-                            <Panel eventKey={i}>
-                                <Panel.Title id="listTitle" className="almostBlack-text slantTitle link" toggle><span id="listTitleBorder">{job.title}</span></Panel.Title>
-                                <div className="slantInfoWrapper almostBlack-text">
-                                    <Panel.Title id="oneLineTitle" className="infoWords link" toggle>{job.oneLine}</Panel.Title>
+                {this.state.jobs.slice(0, 2).map((job, i) => (
+                    <div className="jobWrapper">
+                        <Panel eventKey={i}>
+                            <Panel.Title id="listTitle" className="almostBlack-text slantTitle link" toggle><span id="listTitleBorder">{job.title}</span></Panel.Title>
+                            <div className="slantInfoWrapper almostBlack-text">
+                                <Panel.Title id="oneLineTitle" className="infoWords link" toggle>{job.oneLine}</Panel.Title>
 
-                                    <Panel.Body collapsible>
-                                        <div>{job.description}</div>
-                                        <Link to={"/joinus/" + job.button} className="applyButton jobButton">see available {job.button} positions</Link >
-                                        <Link to={"/joinus/remind"} className="applyButton jobButton">remind me to apply later</Link >
-                                    </Panel.Body>
-                                </div>
-                            </Panel>
-                        </div>
-                        : <div></div>
+                                <Panel.Body collapsible>
+                                    <div>{job.description}</div>
+                                    <Link to={"/joinus/" + job.button} className="applyButton jobButton">see available {job.button} positions</Link >
+                                    <Link to={"/joinus/remind"} className="applyButton jobButton">remind me to apply later</Link >
+                                </Panel.Body>
+                            </div>
+                        </Panel>
+                    </div>
                 ))}
 
                 <div className="slantUp green-background">
                     <div className="unSkew">
-                        {this.state.jobs.map((job, i) => (
-                            (i === 2 || i === 3)
-                                ? <div className="jobWrapper">
-                                    <Panel eventKey={i}>
-                                        <Panel.Title id="listTitleWhite" className="slateWhite-text slantTitle link" toggle><span id="listTitleWhiteBorder">{job.title}</span></Panel.Title>
-                                        <div className="slantInfoWrapper slateWhite-text">
-                                            <Panel.Title id="oneLineTitleWhite" className="infoWords link" toggle>{job.oneLine}</Panel.Title>
+                        {this.state.jobs.slice(2, 4).map((job, i) => (
+                            <div className="jobWrapper">
+                                <Panel eventKey={i}>
+                                    <Panel.Title id="listTitleWhite" className="slateWhite-text slantTitle link" toggle><span id="listTitleWhiteBorder">{job.title}</span></Panel.Title>
+                                    <div className="slantInfoWrapper slateWhite-text">
+                                        <Panel.Title id="oneLineTitleWhite" className="infoWords link" toggle>{job.oneLine}</Panel.Title>
 
-                                            <Panel.Body collapsible>
-                                                <div id="oneLineTitleWhite">{job.description}</div>
-                                                <Link to={"/joinus/" + job.button} className="applyButton jobButton">see available {job.button} positions</Link >
-                                                <Link to={"/joinus/remind"} className="applyButton jobButton">remind me to apply later</Link >
-                                            </Panel.Body>
-                                        </div>
-                                    </Panel>
-                                </div>
-                                : <div></div>
+                                        <Panel.Body collapsible>
+                                            <div id="oneLineTitleWhite">{job.description}</div>
+                                            <Link to={"/joinus/" + job.button} className="applyButton jobButton">see available {job.button} positions</Link >
+                                            <Link to={"/joinus/remind"} className="applyButton jobButton">remind me to apply later</Link >
+                                        </Panel.Body>
+                                    </div>
+                                </Panel>
+                            </div>
                         ))}
                     </div>
                 </div>
 
-                {this.state.jobs.map((job, i) => (
-                    (i === 4 || i === 5)
-                        ? <div className="jobWrapper">
-                            <Panel eventKey={i}>
-                                <Panel.Title id="listTitle" className="almostBlack-text slantTitle link" toggle><span id="listTitleBorder">{job.title}</span></Panel.Title>
-                                <div className="slantInfoWrapper almostBlack-text">
-                                    <Panel.Title id="oneLineTitle" className="infoWords link" toggle>{job.oneLine}</Panel.Title>
+                {this.state.jobs.slice(4, 6).map((job, i) => (
+                    <div className="jobWrapper">
+                        <Panel eventKey={i}>
+                            <Panel.Title id="listTitle" className="almostBlack-text slantTitle link" toggle><span id="listTitleBorder">{job.title}</span></Panel.Title>
+                            <div className="slantInfoWrapper almostBlack-text">
+                                <Panel.Title id="oneLineTitle" className="infoWords link" toggle>{job.oneLine}</Panel.Title>
 
-                                    <Panel.Body collapsible>
-                                        <div>{job.description}</div>
-                                        <Link to={"/joinus/" + job.button} className="applyButton jobButton">see available {job.button} positions</Link >
-                                        <Link to={"/joinus/remind"} className="applyButton jobButton">remind me to apply later</Link >
-                                    </Panel.Body>
-                                </div>
-                            </Panel>
-                        </div>
-                        : <div></div>
+                                <Panel.Body collapsible>
+                                    <div>{job.description}</div>
+                                    <Link to={"/joinus/" + job.button} className="applyButton jobButton">see available {job.button} positions</Link >
+                                    <Link to={"/joinus/remind"} className="applyButton jobButton">remind me to apply later</Link >
+                                </Panel.Body>
+                            </div>
+                        </Panel>
+                    </div>
                 ))}
 
                 <div className="slantDown salmon-background">
                     <div className="unSkewDown">
-                        {this.state.jobs.map((job, i) => (
-                            (i === 6 || i === 7 || i === 8)
-                                ? <div className="jobWrapper">
-                                    <Panel eventKey={i}>
-                                        <Panel.Title id="listTitleWhite" className="slateWhite-text slantTitle link" toggle><span id="listTitleWhiteBorder">{job.title}</span></Panel.Title>
-                                        <div className="slantInfoWrapper slateWhite-text">
-                                            <Panel.Title id="oneLineTitleWhite" className="infoWords link" toggle>{job.oneLine}</Panel.Title>
+                        {this.state.jobs.slice(6).map((job, i) => (
+                            <div className="jobWrapper">
+                                <Panel eventKey={i}>
+                                    <Panel.Title id="listTitleWhite" className="slateWhite-text slantTitle link" toggle><span id="listTitleWhiteBorder">{job.title}</span></Panel.Title>
+                                    <div className="slantInfoWrapper slateWhite-text">
+                                        <Panel.Title id="oneLineTitleWhite" className="infoWords link" toggle>{job.oneLine}</Panel.Title>
 
-                                            <Panel.Body collapsible>
-                                                <div id="oneLineTitleWhite">{job.description}</div>
-                                                <Link to={"/joinus/" + job.button} className="applyButton jobButton">see available {job.button} positions</Link >
-                                                <Link to={"/joinus/remind"} className="applyButton jobButton">remind me to apply later</Link >
-                                            </Panel.Body>
-                                        </div>
-                                    </Panel>
-                                </div>
-                                : <div></div>
+                                        <Panel.Body collapsible>
+                                            <div id="oneLineTitleWhite">{job.description}</div>
+                                            <Link to={"/joinus/" + job.button} className="applyButton jobButton">see available {job.button} positions</Link >
+                                            <Link to={"/joinus/remind"} className="applyButton jobButton">remind me to apply later</Link >
+                                        </Panel.Body>
+                                    </div>
+                                </Panel>
+                            </div>
                         ))}
                     </div>
                 </div>
